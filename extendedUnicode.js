c430611c5a552374e66460e5b9d6196be4b24f87
chaowen guo <chaowen.guo1@gmail.com>
2021-02-12 07:37:52 -0800

    Update extendedUnicode.js

--- extendedUnicode.js
+++ extendedUnicode.js
@@ -1,2 +1,2 @@
-console.assert(Object.is(String.fromCodePoint(0x10035), '\u{10035}'))
-console.assert(Object.is('\u{10035}'.codePointAt(0).toString(16), '10035'))
+globalThis.console.assert(globalThis.Object.is(globalThis.String.fromCodePoint(0x10035), '\u{10035}'))
+globalThis.console.assert(globalThis.Object.is('\u{10035}'.codePointAt(0).toString(16), '10035'))
