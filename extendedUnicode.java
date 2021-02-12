91d18cc568067146eea1cdb6c2cfae494eb8215c
chaowen guo <chaowen.guo1@gmail.com>
2021-02-12 09:43:29 -0800

    Update extendedUnicode.java

--- extendedUnicode.java
+++ extendedUnicode.java
@@ -2,6 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println(new String(new int[]{0x10035}, 0, 1));
+        System.out.println(Character.toString(0x10035));
     }
 }
