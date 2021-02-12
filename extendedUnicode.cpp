58a4f5326a389e626fd4ffbce31a86ec02f5b56a
chaowen guo <chaowen.guo1@gmail.com>
2021-02-12 09:13:08 -0800

    Update extendedUnicode.cpp

--- extendedUnicode.cpp
+++ extendedUnicode.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-    static_assert(static_cast<unsigned>(U'\U00010035') == 10035);
-    static_assert(static_cast<char32_t>(10035) == U'\U00010035');
+    //static_assert(static_cast<unsigned>(U'\U00010035') == 10035);
+    //static_assert(static_cast<char32_t>(10035) == U'\U00010035');
     std::cout << "\U00010035" << std::endl;
+    std::cout << static_cast<unsigned>(U'\U00010035') << std::endl;
 }
